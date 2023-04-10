0cc8de50a883031ac1c5535a1b815053fcbfe989
vansh <vanshbaghel912@gmail.com>
2023-04-10 17:43:11 +0530

    Added a new function

--- contracts/money.sol
+++ contracts/money.sol
@@ -10,11 +10,17 @@ contract withDrawalMoney{
     }
 
     function withdrawAll() public {
+        // This will transfer all the money to the acc which uses this function.
         address payable to = payable(msg.sender);
         to.transfer(getContractBalance());
     }
 
-    function deposit() public payable{
+    function withdrawAllToSpecifiedAddress(address payable _to) public {
+        // This will transfer all the money to the specified address.
+        _to.transfer(getContractBalance());
+    }
+
+        function deposit() public payable{
         totalBalancedReceived += msg.value;
     }
 }
